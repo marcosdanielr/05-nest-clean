f5bb1b7cc08d826291a0e5931228a605c7eaf99c
marcosdanielr <marcosdaniel99@protonmail.com>
2023-10-21 03:14:11 -0300

    CHORE: prisma.service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { PrismaService } from './prisma/prisma.service'
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
