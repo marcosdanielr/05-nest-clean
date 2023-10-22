0a62d2190e9ae4ee8d66bae1c38327e7a540e07a
marcosdanielr <marcosdaniel99@protonmail.com>
2023-10-22 03:15:49 -0300

    feat: create question

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, UseGuards, UsePipes } from '@nestjs/common'
+import { Body, Controller, Post, UseGuards, UsePipes } from '@nestjs/common'
 import { CurrentUser } from 'src/auth/current-user-decorator'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 import { ZodValidationPipe } from 'src/pipes/zod-validation.pipe'
@@ -6,8 +6,14 @@ import { PrismaService } from 'src/prisma/prisma.service'
 import { z } from 'zod'
 
 import type { UserPayload } from 'src/auth/jwt.strategy'
+import { normalize } from 'path'
 
-const createQuestionBodySchema = z.object({})
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema)
 
 type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
 
@@ -17,11 +23,33 @@ export class CreateQuestionController {
   constructor(private prisma: PrismaService) {}
 
   @Post()
-  @UsePipes(new ZodValidationPipe(createQuestionBodySchema))
-  async handle(@CurrentUser() user: UserPayload) {
-    console.log({
-      user: user.sub,
+  async handle(
+    @Body(bodyValidationPipe) body: CreateQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+  ) {
+    const { title, content } = body
+    const slug = this.convertoSlug(title)
+
+    const userId = user.sub
+
+    await this.prisma.question.create({
+      data: {
+        authorId: userId,
+        title,
+        content,
+        slug,
+      },
     })
-    return 'ok'
+  }
+
+  private convertoSlug(title: string): string {
+    const normalized = title.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+
+    const slug = normalized
+      .toLowerCase()
+      .replace(/[^a-z0-9]+/g, '-')
+      .replace(/(^-|-$)/g, '')
+
+    return slug
   }
 }
